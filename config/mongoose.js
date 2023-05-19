2c7d288fd090cd1c8d43474a1554a1be6abfa56e
KRISHNA NAYAK <krishnakantn76@gmail.com>
2023-05-19 14:19:07 +0530

    mongo atlas configured

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,14 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Simplifi_Otp');
+
+
+const connectionParams={
+            useNewURLParser:true,
+            useUnifiedTopology:true
+        }
+
+mongoose.connect('mongodb+srv://d52306406:PQGvxU5GTYGt2xQZ@cluster0.foe8kyf.mongodb.net/',connectionParams);
+
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console.log('error connecting to db')));
