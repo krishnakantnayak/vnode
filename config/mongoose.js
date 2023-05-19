81f913c59a472b94a12c036956c719e99850eedd
KRISHNA NAYAK <krishnakantn76@gmail.com>
2023-05-19 14:55:05 +0530

    sync to master

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,8 +7,8 @@ const connectionParams={
             useUnifiedTopology:true
         }
 
-mongoose.connect('mongodb+srv://d52306406:PQGvxU5GTYGt2xQZ@cluster0.foe8kyf.mongodb.net/',connectionParams);
-
+// mongoose.connect('mongodb+srv://d52306406:PQGvxU5GTYGt2xQZ@cluster0.foe8kyf.mongodb.net/',connectionParams);
+mongoose.connect(process.env.MONGODB_URI);
 const db=mongoose.connection;
 
 db.on('error',console.error.bind(console.log('error connecting to db')));
--- models/userCred.js
+++ models/userCred.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userCredSchema= mongoose.Schema({
     email:{
         type:String,
+        unique: true,
         required:true
     },
     otp:{
