811b4428dc8ece8764d1096dab124035cd649cc6
KRISHNA NAYAK <krishnakantn76@gmail.com>
2023-05-19 20:19:45 +0530

    if else

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,22 +1,41 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 
+if (process.env.MONGODB_URI) {
+    const connectionParams = {
+        useNewURLParser: true,
+        useUnifiedTopology: true
+    }
 
-const connectionParams={
-            useNewURLParser:true,
-            useUnifiedTopology:true
-        }
+    try {
+        mongoose.connect(process.env.MONGODB_URI, connectionParams);
+        console.log(process.env.mongoDBUrl);
+        // mongoose.connect(process.env.MONGODB_URI);
+        const db = mongoose.connection;
 
+        db.on('error', console.error.bind(console.log('error connecting to db')));
 
+        db.once('open', function () {
+            console.log('storing records at mongoDB');
+        })
+    }
+    catch(err){
+        console.log('#####',err);
+    }
+    
+}
+else {
+    const connectionParams = {
+        useNewURLParser: true,
+        useUnifiedTopology: true
+    }
+    mongoose.connect(process.env.mongoDBUrl, connectionParams);
+    
+    const db = mongoose.connection;
 
-mongoose.connect(process.env.mongoDBUrl,connectionParams);
-console.log(process.env.mongoDBUrl);
+    db.on('error', console.error.bind(console.log('error connecting to db')));
 
-// mongoose.connect(process.env.MONGODB_URI);
-const db=mongoose.connection;
-
-db.on('error',console.error.bind(console.log('error connecting to db')));
-
-db.once('open',function(){
-    console.log('storing records at mongoDB');
-})
+    db.once('open', function () {
+        console.log('storing records at mongoDB');
+    })
+}
