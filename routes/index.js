1dde72c667cd021db3cc82be240705eaebc856e2
KRISHNA NAYAK <krishnakantn76@gmail.com>
2023-05-19 20:40:42 +0530

    reducepath

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const router=express.Router();
 
 
 router.use('/api', require('./api'));
+const getOTPController = require("../controllers/getOTPController");
+const getJWTTokenController=require("../controllers/getJWTController");
+
+
+router.get('/getOTP', getOTPController.getotp);
+router.post('/getJWTToken',getJWTTokenController.createSession);
+
 router.get('/',(req,res)=>{
     return res.send('root');
 })
