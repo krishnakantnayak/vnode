dedffeed9d2bc810e9090a905a8b832d06aa8912
KRISHNA NAYAK <krishnakantn76@gmail.com>
2023-05-19 20:24:37 +0530

    ll

--- config/mongoose.js
+++ config/mongoose.js
@@ -13,7 +13,11 @@ if (process.env.MONGODB_URI) {
         // mongoose.connect(process.env.MONGODB_URI);
         const db = mongoose.connection;
 
-        db.on('error', console.error.bind(console.log('error connecting to db')));
+        db.on('error', function (err) {
+            connected=false;
+            console.log('error connection to mongo server!');
+            console.log(err);
+        });
 
         db.once('open', function () {
             console.log('storing records at mongoDB');
